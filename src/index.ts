243bceed0d2f2ca452813b6d428ff6e43e06fcd7
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2024-01-14 23:31:11 +0700

    feat: add pokemons query

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,41 @@
+import apollo, { gql } from "@elysiajs/apollo";
 import { Elysia } from "elysia";
+import { PokedexApi } from "./pokedex-datasource";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    apollo({
+      typeDefs: gql`
+        type Pokemon {
+          img: String!
+          name: String!
+        }
+
+        type Query {
+          pokemons: [Pokemon!]
+        }
+      `,
+      resolvers: {
+        Query: {
+          pokemons: (
+            _,
+            __,
+            { dataSources }: { dataSources: { pokedexApi: PokedexApi } }
+          ) => {
+            return dataSources.pokedexApi.getPokemons();
+          },
+        },
+      },
+      context: async () => {
+        return {
+          dataSources: {
+            pokedexApi: new PokedexApi(),
+          },
+        };
+      },
+    })
+  )
+  .listen(8080);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
---
+++ src/pokedex-datasource.ts
@@ -0,0 +1,9 @@
+import { RESTDataSource } from "@apollo/datasource-rest";
+
+export class PokedexApi extends RESTDataSource {
+  baseURL?: string | undefined = "https://pokedex-workshop-api.vercel.app/api/";
+
+  async getPokemons() {
+    return this.get("pokemons");
+  }
+}
